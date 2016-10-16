c13756714df704292fdb97158d9124c758e3ee7b
mtask <taskinen.miikka@gmail.com>
2016-10-16 10:20:44 +0300

    scripts

--- multissh_scripts/debian_servers_example.sh
+++ multissh_scripts/debian_servers_example.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Example for Debian/Ubuntu servers in multissh.conf.
+
 # Run updates
 # Check disk space
 # Check auth log for recent password changes
 # Check uptime
+# 
+
 cd ..
 ./multissh.py -c "apt-get -y update > /dev/null && apt-get -y upgrade > /dev/null"
 echo -e "\n"
@@ -12,3 +16,4 @@ echo -e "\n"
 ./multissh.py -c 'grep passwd /var/log/auth.log'
 echo -e "\n" 
 ./multissh.py -c "uptime"
+
